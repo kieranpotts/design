de3e12f32d4eacc774d9fb6869558fb0c8c6ceb1
Kieran Potts <7712355+kieranpotts@users.noreply.github.com>
2026-06-28 14:48:29 +0100

    chore: update commit validation rules

--- .hooks/validate_commit_message.py
+++ .hooks/validate_commit_message.py
@@ -28,7 +28,7 @@ VALID_TYPES = [
     "design",
 ]
 
-PATTERN = rf"^({'|'.join(VALID_TYPES)}): [a-z].*"
+PATTERN = rf"^({'|'.join(VALID_TYPES)}): [a-z0-9].*"
 
 
 def validate_commit_message(message: str) -> bool:
